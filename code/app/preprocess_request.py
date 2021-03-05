bc0ff24a647562629ed1b2ef06c9d19c7d1ad30b
Jorge Alberto Flores Sanchez <jorge.alberto.flores.sanchez@es.ey.com>
2021-03-05 20:45:17 +0100

    Refactoring code

--- code/app/preprocess_request.py
+++ code/app/preprocess_request.py
@@ -1,17 +1,28 @@
 import re
 
+
 def clean_text(text):
-    text = re.sub(r'[^\w\s]','',text)
-    text = re.sub('\w*\d\w*','', text)
+    """
+    Clean text from request
+    :param text: text from request
+    :return: clean text
+    """
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'\w*\d\w*', '', text)
     text = text.lower()
     token_text = text.split()
-    token_text = [word for word in token_text if len(word)>3]
+    token_text = [word for word in token_text if len(word) > 3]
 
     text_clean = " ".join(token_text)
     return text_clean
 
+
 def data_lstm(tfidf):
+    """
+    Transform data to the input model
+    :param tfidf: text format in tfidf to transform as input model
+    :return:
+    """
     
     text_lstm = tfidf.toarray().reshape(tfidf.shape[0], 1, tfidf.shape[1])
     return text_lstm
-
--- code/app/server.py
+++ code/app/server.py
@@ -1,41 +1,39 @@
-import json
 import logging
 import tornado.web
 import os
 import sys
-import pandas as pd
 import numpy as np
 import pickle
-import h5py
-from keras.models import load_model
 from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
-from tornado import gen
-from tornado.concurrent import run_on_executor
-from concurrent.futures import ThreadPoolExecutor
+from keras.models import load_model
+
 sys.path.append(os.path.realpath('../'))
 sys.path.append(os.path.realpath('../../'))
-from preprocess_request import clean_text,data_lstm
-from keras.models import load_model
+from preprocess_request import clean_text, data_lstm
+
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s[%(name)s][%(levelname)s] %(message)s',
                     datefmt='[%Y-%m-%d][%H:%M:%S]')
+
 logger = logging.getLogger(__name__)
 
 define('port', default=9000, help='port to listen on')
 
+
 class IndexHandler(RequestHandler):
     def get(self):
         self.render('../../templates/index.html')
+
 
 class Predict(RequestHandler):
     def post(self):
         text = self.get_argument('text', None)
         text = clean_text(text)
         
-        try :
+        try:
             models
         except Exception:
             models = self.load_models()
@@ -48,7 +46,7 @@ class Predict(RequestHandler):
         result = models['label'].inverse_transform(np.argmax(predict_text, axis=1))
         logger.info(result)
 
-        self.render('../../templates/results.html',data=result[0])
+        self.render('../../templates/results.html', data=result[0])
 
     def load_models(self):
         models = dict()
@@ -56,14 +54,14 @@ class Predict(RequestHandler):
 
         models['tfidf'] = pickle.load(open(path_models+'tfidf.pickle', 'rb'))
         models['lstm'] = load_model(path_models+"lstm.h5")
-        models['label'] = pickle.load(open(path_models+'label.pickle','rb'))
+        models['label'] = pickle.load(open(path_models+'label.pickle', 'rb'))
         
         return models
 
-def make_app():
-    urls = [("/",IndexHandler),("/results",Predict)]
-    return Application(urls, debug=True)
 
+def make_app():
+    urls = [("/", IndexHandler), ("/results", Predict)]
+    return Application(urls, debug=True)
 
 
 if __name__ == '__main__':
@@ -72,8 +70,3 @@ if __name__ == '__main__':
     logger.info('Serving Runing')
     logger.info('http://localhost:9000/')
     IOLoop.current().start()
-    
-
-
-
-        
